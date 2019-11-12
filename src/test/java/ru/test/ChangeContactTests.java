49c6bf99370ca1c0399f2e9baeaee4b01706babf
YuliaProkopeva <42172341+YuliaProkopeva@users.noreply.github.com>
2019-11-12 17:37:08 +0400

    Update ChangeContactTests.java

--- src/test/java/ru/test/ChangeContactTests.java
+++ src/test/java/ru/test/ChangeContactTests.java
@@ -18,6 +18,7 @@ public class ChangeContactTests extends TestBase {
         app.getChangesHelper().clickEditContact();
         app.getContactHelper().fillContactName(new ContactUser("test3", "test7", "test8"));
         app.getChangesHelper().pressUpdateButton();
+        app.getContactHelper().returnToContactPage();
         app.getChangesHelper().clickModifyContact();
         app.getContactHelper().fillPhone(new ContactPhones("test12", "test1", "test4"));
         app.getChangesHelper().pressUpdateButton();
